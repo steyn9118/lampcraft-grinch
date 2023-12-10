5599441055faa8c7644f15fc64652f049e83ac7d
Steyn91 <steyn9118@gmail.com>
2023-12-11 01:56:44 +0700

    init

---
+++ src/main/java/steyn91/grinchplugin/GrinchPlugin.java
@@ -0,0 +1,17 @@
+package steyn91.grinchplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GrinchPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
